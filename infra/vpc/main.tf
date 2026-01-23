57e96b302523feee42b68d9133931b7505b8efab
Riz Josel Urian <rurian@apple.com>
2026-01-23 20:25:42 +0800

    add vpc

---
+++ infra/vpc/main.tf
@@ -0,0 +1,34 @@
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tag {
+    Name = "eks"
+  }
+}
+
+resource "aws_subnet" "eks_public_subnet" {
+  vpc_id     = aws_vpc.eks_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "eks_private_subnet" {
+  vpc_id     = aws_vpc.eks_vpc.id
+  cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+resource "aws_route_table" "eks_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+}
+
+resource "aws_route_table_association" "eks_public_rt_assoc" {
+  subnet_id      = aws_subnet.eks_public_subnet.id
+  route_table_id = aws_route_table.eks_rt.id
+}
