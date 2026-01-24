085c35cece8f555f3702d72f377a5c2dd719510f
Riz Josel Urian <rurian@apple.com>
2026-01-24 14:46:37 +0800

    update output

--- infra/vpc/output.tf
+++ infra/vpc/output.tf
@@ -3,9 +3,9 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
+  value = [for s in aws_subnet.public : s.id]
 }
 
 output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
+  value = [for s in aws_subnet.private : s.id]
 }
